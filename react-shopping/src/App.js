d55d2e3c98720e6b2045cf3f02cac6b787614d55
Rojin <rojinabraham@gmail.com>
2020-10-21 17:46:54 +0530

    filter-component

--- react-shopping/src/App.js
+++ react-shopping/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import data from "./data.json";
 import Products from "./components/products";
+import Filter from "./components/Filter";
 class App extends React.Component {
   constructor() {
     super();
@@ -9,6 +10,41 @@ class App extends React.Component {
       size: "",
       sort: "",
     };
+    this.sortProducts = this.sortProducts.bind(this);
+    this.filterProducts = this.filterProducts.bind(this);
+  }
+  sortProducts(e) {
+    const sort = e.target.value;
+    this.setState((state) => ({
+      sort,
+      products: state.products
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id < b._id
+            ? 1
+            : -1
+        ),
+    }));
+  }
+  filterProducts(e) {
+    if (e.target.value === "") {
+      this.setState({ size: e.target.value, products: data.products });
+    } else {
+      this.setState({
+        size: e.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(e.target.value) >= 0
+        ),
+      });
+    }
   }
   render() {
     return (
@@ -19,7 +55,14 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main-content">
-              <Products products={this.state.products}></Products>
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              />
+              <Products products={this.state.products} />
             </div>
             <div className="sidebar">Cart Items </div>
           </div>
--- react-shopping/src/components/products.js
+++ react-shopping/src/components/products.js
@@ -4,26 +4,24 @@ import formatCurrency from "../util";
 export default class Products extends Component {
   render() {
     return (
-      <div>
-        <ul className="products">
-          {this.props.products.map((product) => {
-            return (
-              <li key={product._id}>
-                <div className="product">
-                  <a href={"#" + product._id}>
-                    <img src={product.image} alt={product.title} />
-                  </a>
-                  <p>{product.title}</p>
-                  <div className="product-price">
-                    <div>{formatCurrency(product.price)}</div>
-                    <button className="button primary">Add To Cart</button>
-                  </div>
+      <ul className="products">
+        {this.props.products.map((product) => {
+          return (
+            <li key={product._id}>
+              <div className="product">
+                <a href={"#" + product._id}>
+                  <img src={product.image} alt={product.title} />
+                </a>
+                <p>{product.title}</p>
+                <div className="product-price">
+                  <div>{formatCurrency(product.price)}</div>
+                  <button className="button primary">Add To Cart</button>
                 </div>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     );
   }
 }
